53f1d1bdcc5f83f958149d15613eeb9dfedf2bbc
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-16 14:49:53 +0100

    Create collectionCard function and console log with for each loop to see the info

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import { paintingsCollection } from "./collections";
+
+const container = document.querySelector(".container");
+
+function collectionsCard() {
+  paintingsCollection.forEach((painting) => {
+    console.log(painting)
+  })
+}
+collectionsCard()
