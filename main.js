574da2c6579f46dccee3fd980166cdf1ec139b5d
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-16 14:55:56 +0100

    Create paintingCards div

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const container = document.querySelector(".container");
 function collectionsCard() {
   paintingsCollection.forEach((painting) => {
     console.log(painting)
+
+    const paintingCards = document.createElement("div")
+    paintingCards.classList.add('cards')
   })
 }
 collectionsCard()
