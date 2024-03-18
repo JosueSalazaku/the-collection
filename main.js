b9d3f7c1225590df712875b3ce5e3e0e1391c497
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-18 21:21:36 +0100

    Add and display all the painting pictures

--- main.js
+++ main.js
@@ -5,54 +5,48 @@ const displayCards = document.querySelector(".display-cards");
 
 function collectionsCard() {
   paintingsCollection.forEach((painting) => {
-    console.log(painting)
+    console.log(painting);
 
-    const paintingCards = document.createElement("div")
-    paintingCards.classList.add('cards')
+    const paintingCards = document.createElement("div");
+    paintingCards.classList.add("cards");
     container.appendChild(displayCards);
     displayCards.appendChild(paintingCards);
 
-    cardsInfo(painting, paintingCards)
-  })
+    cardsInfo(painting, paintingCards);
+  });
 }
 function cardsImage(painting, paintingCards, cardsInfo) {
-
-  let paintingImg = document.createElement('img');
-  // paintingImg.src = painting.picture;
-
+  let paintingImg = document.createElement("img");
+  paintingImg.src = painting.picture;
   paintingCards.appendChild(paintingImg);
 }
 
-
 function cardsInfo(painting, paintingCards) {
-  let artTitle = document.createElement('p');
+  let artTitle = document.createElement("p");
   artTitle.textContent = painting.title;
-  paintingCards.appendChild(artTitle)
+  paintingCards.appendChild(artTitle);
 
-  let artistName = document.createElement('h1');
+  let artistName = document.createElement("h1");
   artistName.textContent = painting.artist;
   paintingCards.appendChild(artistName);
 
-  let artDescription = document.createElement('p');
+  let artDescription = document.createElement("p");
   artDescription.textContent = painting.description;
   paintingCards.appendChild(artDescription);
 
-  let artStyle = document.createElement('p');
+  let artStyle = document.createElement("p");
   artStyle.textContent = painting.style;
-  paintingCards.appendChild(artStyle)
+  paintingCards.appendChild(artStyle);
 
-  let artDimensions = document.createElement('p');
+  let artDimensions = document.createElement("p");
   artDimensions.textContent = painting.dimensions;
   paintingCards.appendChild(artDimensions);
 
-  let artMedium = document.createElement('p');
+  let artMedium = document.createElement("p");
   artMedium.textContent = painting.medium;
   paintingCards.appendChild(artMedium);
 
-  cardsImage(painting, paintingCards, cardsInfo)
-
+  cardsImage(painting, paintingCards, cardsInfo);
 }
 
-
-
-collectionsCard()
+collectionsCard();
